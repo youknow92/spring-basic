59a5daeb4c488061505acdbb2b25766ce72596a7
youknow92 <dbflwla1@gmail.com>
2021-01-31 22:31:17 +0900

    initial commit

---
+++ src/main/java/com/example/springbasic/SpringConfig.java
@@ -0,0 +1,34 @@
+package com.example.springbasic;
+
+import com.example.springbasic.repository.*;
+import com.example.springbasic.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    //    private final DataSource dataSource;
+//    private final EntityManager em;
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+//    @Bean
+//    public MemberRepository memberRepository() {
+////        return new MemoryMemberRepository();
+////        return new JdbcMemberRepository(dataSource);
+////        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//    }
+}
